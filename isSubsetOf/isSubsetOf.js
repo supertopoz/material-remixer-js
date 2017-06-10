427daf66dd4a2ba5dfae884de17f0fe8a9a7a39e
Jason Allshorn <deepsleepsounds@gmail.com>
2017-06-10 16:05:32 +0900

    added tests and negative test

--- isSubsetOf/isSubsetOf.js
+++ isSubsetOf/isSubsetOf.js
@@ -22,20 +22,39 @@
 */
 
 Array.prototype.isSubsetOf = function (arr) {
-  // your code here
+
   let uniq = Array.from(new Set(this));
   let r = uniq.reduce((ac,x)=> {
     if(arr.indexOf(x) >=0) ac.push(x);
     return ac;
-  },[]).length
+  },[]).length;
   return (r === uniq.length) ? true : false;
 };
 
+var assert = (actual, expected, test) => {
+	if(actual === expected) return "PASSED: " + test;
+	return "FAILED:" +test+" expected:"+expected+" but got:" + actual;
+}
 
 
+// ------------------Test 1 basic ----------------------------------------------------
  let a = ['commit','push']
- console.log(a.isSubsetOf(['commit','rebase','push','blame'])); // true
+ let expected1 = true;
+ let test1 = 'It checks a is a subset of the array';
+ console.log(assert(a.isSubsetOf(['commit','rebase','push','blame']),expected1,test1))
 
- let b = ['merge','reset','reset']
- console.log(b.isSubsetOf(['reset','merge','add','commit'])) // true 
+// ------------------Test 2 dublicated enteries ---------------------------------------------------
+
+ let b = ['merge','reset','reset'];
+ let expected2 = true;
+ let test2 = 'It checks b is a subset of the array then the array contains duplicates';
+ console.log(assert(b.isSubsetOf(['reset','merge','add','commit']),expected2,test2))
+
+ // ------------------Test 3 negative basic test ---------------------------------------------------
+
+ let c = ['bob','steve'];
+ let expected3 = false;
+ let test3 = 'It checks c is NOT a subset of the array';
+ console.log(assert(c.isSubsetOf(['reset','merge','add','commit']),expected3,test3))
+
 
