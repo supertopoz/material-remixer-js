a85ace05c1978a454902d15c288bdb682209ff8b
Jason Allshorn <deepsleepsounds@gmail.com>
2017-06-10 16:24:13 +0900

    added tests including for the bonus points

--- isSubsetOf/isSubsetOf.js
+++ isSubsetOf/isSubsetOf.js
@@ -24,8 +24,14 @@
 Array.prototype.isSubsetOf = function (arr) {
 
   let uniq = Array.from(new Set(this));
+
+  arr = arr.reduce((ac,x)=>{
+    ac.push(JSON.stringify(x));
+    return ac;
+  },[])
+
   let r = uniq.reduce((ac,x)=> {
-    if(arr.indexOf(x) >=0) ac.push(x);
+    if(arr.indexOf(JSON.stringify(x)) >=0) ac.push(x);
     return ac;
   },[]).length;
   return (r === uniq.length) ? true : false;
@@ -56,5 +62,33 @@ var assert = (actual, expected, test) => {
  let expected3 = false;
  let test3 = 'It checks c is NOT a subset of the array';
  console.log(assert(c.isSubsetOf(['reset','merge','add','commit']),expected3,test3))
+
+  // ------------------Test 4 tests for an array with an object ---------------------------------------------------
+
+ let d = [{"name":"bob"},'steve'];
+ let expected4 = true;
+ let test4 = 'It checks d as an object is a subset of the array';
+ console.log(assert(d.isSubsetOf(['reset',{"name":"bob"},'steve','commit']), expected4 , test4))
+
+   // ------------------Test 5 tests for an array with an array ---------------------------------------------------
+
+ let e = [["bill","bob"],'steve'];
+ let expected5 = true;
+ let test5 = 'It checks e as an array is a subset of the array';
+ console.log(assert(e.isSubsetOf(['reset',["bill","bob"],'steve','commit']), expected5 , test5))
+
+ // ------------------Test 6 negative test for an array with an object ---------------------------------------------------
+
+ let f = [{"name":"bob"},'steve'];
+ let expected6 = false;
+ let test6 = 'It negative checks f is not a subset of the array where f contains an object';
+ console.log(assert(f.isSubsetOf(['reset',{"name":"bob","name":"cooliloo"},'steve','commit']), expected6 , test6))
+
+ // ------------------Test 7 negative test for array with an array ---------------------------------------------------
+
+ let g = [["bill","bob"],'steve'];
+ let expected7 = false;
+ let test7 = 'It checks g as an array is not a subset of the array';
+ console.log(assert(g.isSubsetOf(['reset',["bill"],'steve','commit']), expected7 , test7))
 
 
