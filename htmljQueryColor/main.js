87b595442e4f71cf21203e21260958c5e5a77f0a
Jason Allshorn <deepsleepsounds@gmail.com>
2017-07-07 11:05:48 +0900

    completed binary search and flashing words

--- htmljQueryColor/main.js
+++ htmljQueryColor/main.js
@@ -7,10 +7,32 @@ $(function() {
 
   // TODO: your code here!
 
-
+  $('p').each(function(){
+    var text = $(this).text().split(' ').join(' </span><span>');
+    $(this).empty().html(text);
+  });
   // --------------STEP 2--------------
   // Next, change spans to random colors, once per second
 
   // TODO: your code here!
+  function randomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  } 
 
+  $("span").each(function(index) {        
+  	this.flash = function(){
+  	  setTimeout(function(){
+        $(this).css("color",randomColor())
+        this.flash();
+  	  }.bind(this),1000)		
+  	}
+  	this.flash();
+  });
 });
+
+  
--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -14,3 +14,15 @@ console.log(largestProductOfThree([2,1,3,7]));
 console.log(largestProductOfThree([-2,-1,-3,-7]));
 
 
+
+var largestProductOfThree = function(array){
+	var sortedNums = array.slice().sort(function(a,b){
+		return a - b;
+	})
+	var n = sortedNums.length;
+	var firstProduct = sortedNums[0]*sortedNums[1]*sortedNums[2]
+	var larstProduct = sortedNums[n-1]*sortedNums[n-2]*sortedNums[n-3]
+	return Math.max(firstProduct, larstProduct)
+}
+
+// O(n log n)
