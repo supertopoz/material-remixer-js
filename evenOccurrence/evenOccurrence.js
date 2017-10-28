6504fa22b41c4ec6a2685bedbd2b30c3de9f7693
supertopoz <deepsleepsounds@gmail.com>
2017-10-29 01:50:22 +0900

    finished evenOccurence

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -11,5 +11,16 @@
 */
 
 var evenOccurrence = function(arr) {
-  // Your code here.
+  
+  let final = arr.reduce((arc,item)=>{
+    (arc[item] === undefined)? arc[item] = 1 : arc[item] += 1
+    return arc
+  },{})
+  for (var i = 0; i<arr.length; i ++){
+  	if (final[arr[i]]%2 === 0) return arr[i]
+  }
 };
+
+var onlyEven = evenOccurrence([1, 7, 2, 4, 5, 6,  8, 9, 6, 4]);
+
+console.log(onlyEven);
